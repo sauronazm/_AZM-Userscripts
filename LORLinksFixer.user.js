c864b02743d6a2a938e6ce7ceab3b06d812de33f
sauronazm@gmail.com <sauronazm@gmail.com>
2013-02-12 01:07:59 +0400

    Made some changes to WikipediaImagesCleaner.user.js. Added LORLinksFixer.user.js.

--- LORLinksFixer.user.js
+++ LORLinksFixer.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name           Wikipedia's images cleaner
+// @name           LOR Links Fixer
 // @namespace      SKIPPED
 // @updateURL      SKIPPED
 // @require        SKIPPED
 // @author         Sauron_AZM (sauronazm@gmail.com)
-// @description    Cleans images URLs on wikipedia (Deletes "?userlang=ru" from URL). 
-// @include        http://ru.wikipedia.org/*
-// @include        https://ru.wikipedia.org/*
-// @include        http://www.ru.wikipedia.org/*
-// @include        https://www.ru.wikipedia.org/*
+// @description    Deletes "#comments" from links on linux.org.ru web site.  
+// @include        http://linux.org.ru/*
+// @include        http://www.linux.org.ru/*
+// @include        https://linux.org.ru/*
+// @include        https://www.linux.org.ru/*
 // @grant          SKIPPED
-// @version        1.0.1
+// @version        1.0.0
 // @source         SKIPPED
 
 // ==/UserScript==
@@ -19,8 +19,8 @@ if (typeof _AZM != "object") {
   _AZM = {};
 }
 
-_AZM.DeleteUselangParameter = function() {
-  var badLinkPart = "?uselang=";
+_AZM.FixLORLinks = function() {
+  var badLinkPart = "#comments";
   var links = document.getElementsByTagName("a");
   for (var i = 0; i < links.length; i++) {
     if (~links[i].href.toLowerCase().indexOf(badLinkPart)) {
@@ -29,4 +29,4 @@ _AZM.DeleteUselangParameter = function() {
   }
 }
 
-setTimeout(_AZM.DeleteUselangParameter, 3000);
+setTimeout(_AZM.FixLORLinks, 2000);
